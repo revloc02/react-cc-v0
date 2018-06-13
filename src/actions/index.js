cc85a454ac412bc82276116a6e3b8f5c5c01c284
Forest Colver <fcolver@ldschurch.org>
2018-06-13 08:48:14 -0600

    adding some creature data actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ export const decountAction = { type: "DECOUNT" };
 
 export const selectCreature = creature => ({ type: "SELECT_CREATURE", creature });
 
+// template actions
 export const addMighty = { type: "ADD_MIGHTY" };
 export const removeMighty = { type: "REMOVE_MIGHTY" };
 
@@ -12,3 +13,20 @@ export const decreaseAdvanced = { type: "DECREASE_ADVANCED" };
 
 export const increaseGiant = { type: "INCREASE_GIANT" };
 export const decreaseGiant = { type: "DECREASE_GIANT" };
+
+// creature data actions
+export const requestCreatureData = () => ({ type: "FETCH_CREATURE_DATA_REQUEST" });
+export const storeCreatureData = creature => ({ type: "FETCH_CREATURE_DATA_SUCCESS", creature });
+export const errorCreatureData = creature => ({ type: "FETCH_CREATURE_DATA_FAILURE", creature });
+export const fetchCreatureData = dispatch => {
+    dispatch(requestCreatureData());
+    return fetch(API, {
+      headers: {
+        Authorization:
+          "Basic " + btoa("tqadmin:1f3001df-4ebe-4c23-b110-f84fbd4eaa0f")
+      }
+    })
+      .then(res => (res.ok ? res.json() : Promise.reject(res)))
+      .then(json => dispatch(receiveAddressList(json)))
+      .catch(resErr => dispatch(errorAddressList(resErr)));
+  };
